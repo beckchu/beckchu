7220e4af97192945f238ab08b8ef5abf20fba924
Codingbug <240615534@qq.com>
2020-10-19 14:43:12 +0800

    Create EventLoopThread.cc

---
+++ net/EventLoopThread.cc
@@ -0,0 +1,70 @@
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#include <muduo/net/EventLoopThread.h>
+
+#include <muduo/net/EventLoop.h>
+
+#include <boost/bind.hpp>
+
+using namespace muduo;
+using namespace muduo::net;
+
+
+EventLoopThread::EventLoopThread(const ThreadInitCallback& cb)
+  : loop_(NULL),
+    exiting_(false),
+    thread_(boost::bind(&EventLoopThread::threadFunc, this)),
+    mutex_(),
+    cond_(mutex_),
+    callback_(cb)
+{
+}
+
+EventLoopThread::~EventLoopThread()
+{
+  exiting_ = true;
+  loop_->quit();
+  thread_.join();
+}
+
+EventLoop* EventLoopThread::startLoop()
+{
+  assert(!thread_.started());
+  thread_.start();//线程threadFunc()创建loop
+
+  {
+    MutexLockGuard lock(mutex_);
+    while (loop_ == NULL)
+    {
+      cond_.wait();
+    }
+  }
+
+  return loop_;
+}
+
+void EventLoopThread::threadFunc()
+{
+  EventLoop loop;
+
+  if (callback_)
+  {
+    callback_(&loop);
+  }
+
+  {
+    MutexLockGuard lock(mutex_);
+    loop_ = &loop;//两个线程的栈数据竟然可以传输
+    cond_.notify();
+  }
+
+  loop.loop();//会在这里停下来，
+  //assert(exiting_);
+}
+
